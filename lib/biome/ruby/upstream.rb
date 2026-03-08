49b2bcfcc0aa17f47b9aa07fd7cc089e12b8c820
Michael Hebblethwaite <32097294+michebble@users.noreply.github.com>
2026-03-08 14:30:15 +0900

    Add support for windows binaries

--- lib/biome/ruby/upstream.rb
+++ lib/biome/ruby/upstream.rb
@@ -7,12 +7,14 @@ module Biome
 
       # rubygems platform name => upstream release filename
       NATIVE_PLATFORMS = {
+        'aarch64-linux-gnu' => 'biome-linux-arm64',
+        'aarch64-linux-musl' => 'biome-linux-arm64-musl',
         'arm64-darwin' => 'biome-darwin-arm64',
+        'x64-mingw-ucrt' => 'biome-win32-x64.exe',
+        'x64-mingw32' => 'biome-win32-x64.exe',
         'x86_64-darwin' => 'biome-darwin-x64',
         'x86_64-linux-gnu' => 'biome-linux-x64',
-        'x86_64-linux-musl' => 'biome-linux-x64-musl',
-        'aarch64-linux-gnu' => 'biome-linux-arm64',
-        'aarch64-linux-musl' => 'biome-linux-arm64-musl'
+        'x86_64-linux-musl' => 'biome-linux-x64-musl'
       }.freeze
     end
   end
