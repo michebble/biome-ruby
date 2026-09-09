527952d89fb131e40018c33d727300d2ea2afe73
Michael Hebblethwaite <32097294+michebble@users.noreply.github.com>
2026-09-09 17:10:04 +0900

    dep: update to Biome CLI v2.5.12

--- lib/biome/ruby/upstream.rb
+++ lib/biome/ruby/upstream.rb
@@ -3,7 +3,7 @@
 module Biome
   module Ruby
     module Upstream
-      VERSION = '2.5.11'
+      VERSION = '2.5.12'
 
       # rubygems platform name => upstream release filename
       NATIVE_PLATFORMS = {
